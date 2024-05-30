6da6d6b5e83b8caa8275db4e2d00bf6b9206dea6
GitHub Action <action@github.com>
2024-05-30 13:08:18 +0000

    Update formula for release

--- Formula/holmesgpt.rb
+++ Formula/holmesgpt.rb
@@ -3,11 +3,11 @@ class Holmesgpt < Formula
     homepage "https://github.com/robusta-dev/experimental-ai-investigation"
   
     if OS.mac?
-        url "https://github.com/robusta-dev/experimental-ai-investigation/releases/download/0.3/main-macos-latest-0.3.zip"
-        sha256 "fb589b39e987340c11df873bdccd163f53913f8bb585e76b1f193f374313fc4d"
+        url "https://github.com/robusta-dev/experimental-ai-investigation/releases/download/0.3.1-alpha.1/main-macos-latest-0.3.1-alpha.1.zip"
+        sha256 "bce506e78c7b0811f27ee1f340ab1e826dd6d3fc421cb0a49ba75f83a0b48b65"
     elsif OS.linux?
-        url "https://github.com/robusta-dev/experimental-ai-investigation/releases/download/0.3/main-ubuntu-latest-0.3.zip"
-        sha256 "dc6bae6bafd8abc9c1ffb147e6feb23e8a5026be14145aac3f3b0fb16c3a6b04"
+        url "https://github.com/robusta-dev/experimental-ai-investigation/releases/download/0.3.1-alpha.1/main-ubuntu-latest-0.3.1-alpha.1.zip"
+        sha256 ""
     end
   
     def install
