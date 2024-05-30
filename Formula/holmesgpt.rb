0f130d34aafb0d0e01f863de200df611b232a0f7
avi@robusta.dev <avi@robusta.dev>
2024-05-30 17:04:44 +0300

    brew fixes

--- Formula/holmesgpt.rb
+++ Formula/holmesgpt.rb
@@ -3,11 +3,11 @@ class Holmesgpt < Formula
     homepage "https://github.com/robusta-dev/experimental-ai-investigation"
   
     if OS.mac?
-        url "https://github.com/robusta-dev/holmestgpt/releases/download/0.1.0/main-macos-latest-0.1.0.zip"
+        url "https://github.com/robusta-dev/holmesgpt/releases/download/0.1.0/main-macos-latest-0.1.0.zip"
         sha256 "279e7c9fc860087fc174480bcb236f46571a1f58a4a6a8e43ea8234b95fffe67"
     elsif OS.linux?
-        url "https://github.com/robusta-dev/holmestgpt/releases/download/0.1.0/main-ubuntu-latest-0.1.0.zip"
-        sha256 ""
+        url "https://github.com/robusta-dev/holmesgpt/releases/download/0.1.0/main-ubuntu-latest-0.1.0.zip"
+        sha256 "db158ed0aa253ceced96c630484b85e79d2d65e359054041e3b6980f1a229820"
     end
   
     def install
