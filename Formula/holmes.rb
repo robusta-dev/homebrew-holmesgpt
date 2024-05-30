875e6b9ef5dbbb150fa5b6f70a006c33ebe9b65a
avi@robusta.dev <avi@robusta.dev>
2024-05-30 15:38:19 +0300

    added initial file

---
+++ Formula/holmes.rb
@@ -0,0 +1,22 @@
+class Holmes < Formula
+    desc "The Open Source DevOps Assistant"
+    homepage "https://github.com/robusta-dev/experimental-ai-investigation"
+  
+    if OS.mac?
+        url "https://github.com/robusta-dev/experimental-ai-investigation/releases/download/0.3/main-macos-latest-0.3.zip"
+        sha256 "fb589b39e987340c11df873bdccd163f53913f8bb585e76b1f193f374313fc4d"
+    elsif OS.linux?
+        url "https://github.com/robusta-dev/experimental-ai-investigation/releases/download/0.3/main-ubuntu-latest-0.3.zip"
+        sha256 "dc6bae6bafd8abc9c1ffb147e6feb23e8a5026be14145aac3f3b0fb16c3a6b04"
+    end
+  
+    def install
+        libexec.install Dir["*"]
+        bin.write_exec_script (libexec/"holmes")
+    end
+    
+    test do
+        system "#{bin}/holmes", "version"
+    end
+end
+  
