455c634d614459b5c4a0944118cf316d899f42c8
GitHub Action <action@github.com>
2025-02-18 11:44:54 +0000

    Update formula for release

--- Formula/holmesgpt.rb
+++ Formula/holmesgpt.rb
@@ -3,11 +3,11 @@ class Holmesgpt < Formula
     homepage "https://github.com/robusta-dev/experimental-ai-investigation"
   
     if OS.mac?
-        url "https://github.com/robusta-dev/holmesgpt/releases/download/0.9.0/holmes-macos-latest-0.9.0.zip"
-        sha256 "c969431b1da0c92679ab6f3be696ea6f635321e514cee5089b1c853ff5b5f0af"
+        url "https://github.com/robusta-dev/holmesgpt/releases/download/0.10.0/holmes-macos-latest-0.10.0.zip"
+        sha256 "3b6da87e4e216e6cffb313d68efb5ac47f5155914909188e3f296dac771a4014"
     elsif OS.linux?
-        url "https://github.com/robusta-dev/holmesgpt/releases/download/0.9.0/holmes-ubuntu-20.04-0.9.0.zip"
-        sha256 "89ad2abb70748a7fc732997a5c5eced067f640211b8f6113ca0d4fe496e574b2"
+        url "https://github.com/robusta-dev/holmesgpt/releases/download/0.10.0/holmes-ubuntu-20.04-0.10.0.zip"
+        sha256 "73109cfeafa8fbbf18977f85ccc8713bf7dffadcbfce7f14cedf617e291409de"
     end
   
     def install
