f52edefcaf3dfe55e795dcc19c4b5ff819303876
GitHub Action <action@github.com>
2026-08-02 13:05:36 +0000

    Update formula for release 0.38.1

--- Formula/holmesgpt.rb
+++ Formula/holmesgpt.rb
@@ -3,11 +3,11 @@ class Holmesgpt < Formula
     homepage "https://github.com/HolmesGPT/holmesgpt"
   
     if OS.mac?
-        url "https://github.com/HolmesGPT/holmesgpt/releases/download/0.38.0/holmes-darwin-arm64-0.38.0.zip"
-        sha256 "cc6c6c9c0c18a4f0029955b4f5e43431df7e7be1e27ac725733a989699809535"
+        url "https://github.com/HolmesGPT/holmesgpt/releases/download/0.38.1/holmes-darwin-arm64-0.38.1.zip"
+        sha256 "19dad45a2e6ce526657ef37c8c1638b4a83f3de60bb71d81aa1035536bc83c14"
     elsif OS.linux?
-        url "https://github.com/HolmesGPT/holmesgpt/releases/download/0.38.0/holmes-linux-amd64-0.38.0.zip"
-        sha256 "77b29f0509bc466210080fbb4363c535d89a674b37827470530e88d89cc4b0c1"
+        url "https://github.com/HolmesGPT/holmesgpt/releases/download/0.38.1/holmes-linux-amd64-0.38.1.zip"
+        sha256 "29c3c7a41e35b6ac37d67e8d7d6f1179a2190d693c1d8a0823b986a88a48b490"
     end
   
     def install
